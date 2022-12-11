2363c1ff6964107c1ee38d8bdc4a789b821985cc
Pedro Barros <pedrobarros0101@outlook.com>
2022-12-11 12:10:41 -0300

    Adicionando formulario de adicionar participante

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,34 @@
-import { Text, View } from 'react-native';
+import { Text, TextInput, View, TouchableOpacity } from 'react-native';
 import { styles } from './styles';
 
 export default function Home() {
+
+  function handleParticipantAdd(){
+    console.log('CLicou no adicionar');
+  }
   return (
     <View style={styles.container}>
         <Text style={styles.eventName}>Nome do Evento</Text>
-        <Text style={styles.eventDate}>sabado, 10 de dezembro de 2022</Text>
+        
+        <Text style={styles.eventDate}>
+            sabado, 10 de dezembro de 2022
+        </Text>
+
+        <View style={styles.form}>
+            <TextInput 
+                style={styles.input}
+                placeholder="Nome do participante"
+                placeholderTextColor={"#6B6B6B"}
+
+            />
+            <TouchableOpacity 
+            style={styles.button} 
+            onPress={handleParticipantAdd}
+            >
+                <Text style={styles.buttonText}>+</Text>
+            </TouchableOpacity>
+        </View>
+    
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -8,7 +8,7 @@ export const styles = StyleSheet.create({
       padding: 24
     },
     eventName: {
-      color: '#FDFCFE',
+      color: '#FFF',
       fontSize: 24,
       fontWeight: 'bold',
       marginTop: 48
@@ -16,5 +16,33 @@ export const styles = StyleSheet.create({
     eventDate: {
       color: '#6B6B6B',
       fontSize: 16
+    },
+    input: {
+        flex: 1,
+        height: 56,
+        backgroundColor: '#1F1E25',
+        borderRadius: 5,
+        color: '#FFF',
+        padding: 16,
+        fontSize: 16,
+        marginRight: 12
+    },
+    buttonText: {
+        color: '#FFF',
+        fontSize: 24
+    },
+    button: {
+        width:56,
+        height: 56,
+        borderRadius: 5,
+        backgroundColor: '#31CF67',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    form: {
+        flexDirection: 'row',
+        width: '100%',
+        marginTop: 36,
+        marginBottom: 42
     }
   });
