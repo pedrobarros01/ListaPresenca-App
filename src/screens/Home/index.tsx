cbd60697c05473744ec035291f1ff422dd060ac7
Pedro Barros <pedrobarros0101@outlook.com>
2022-12-11 13:33:52 -0300

    Adicionando lista de participante e o card de participante

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,17 @@
-import { Text, TextInput, View, TouchableOpacity } from 'react-native';
+import { Text, TextInput, View, TouchableOpacity, ScrollView, FlatList } from 'react-native';
+import { Participant } from '../../components/Participant';
 import { styles } from './styles';
 
 export default function Home() {
-
+  const participants = ['Nandin', 'Knek', 'Samuca', 'Danik', 'Cintia', 'Joao', 'Leao', 'Miguez', 'Teusma', 'Alice']
   function handleParticipantAdd(){
     console.log('CLicou no adicionar');
   }
+
+  function handleParticipantRemove(name: string){
+    console.log('Removeu participante ' + name);
+  }
+
   return (
     <View style={styles.container}>
         <Text style={styles.eventName}>Nome do Evento</Text>
@@ -28,7 +34,30 @@ export default function Home() {
                 <Text style={styles.buttonText}>+</Text>
             </TouchableOpacity>
         </View>
-    
+        {/* ScrollView e FlatList ativa a rolagem  */}
+        <FlatList 
+          data={[]}
+          keyExtractor={item => item}
+          renderItem={({item}) => (
+            <Participant 
+              key={item}
+              name={item} 
+              onRemove={()=> handleParticipantRemove(item)} 
+              
+              />
+          )}
+          showsVerticalScrollIndicator={false}
+          ListEmptyComponent={() => (
+            <Text style={styles.listEmptyText}>
+              Ninguem chegou no evento ainda? Adicione participantes na sua lista de presença.
+            </Text>
+          )}
+        />
+          {/* {
+            participants.map(participant => (
+              
+            ))
+          } */}
     </View>
   );
 }
